a437f0d52105ded1fbccd087926ce5c04ac0033e
Nicolas Duret <nicolas@avecnous.eu>
2021-02-09 12:51:15 +0100

    Rebase Project

---
+++ docs/precache-manifest.78d981283bb3e3248fc8ae8487d2591c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffefc5e09772cf58c0b3",
+    "url": "/vue-app-2/css/app.08ac570d.css"
+  },
+  {
+    "revision": "06d18f9f1bfa3c7ed3181fe23eb9a93a",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "ffefc5e09772cf58c0b3",
+    "url": "/vue-app-2/js/app.7dbb7630.js"
+  },
+  {
+    "revision": "8c9ead54c6388fbe8a2c",
+    "url": "/vue-app-2/js/chunk-vendors.6fe5dc95.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
